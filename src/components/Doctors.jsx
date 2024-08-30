3b3cc8c8bc1ea7adf4b753591a286d49f28549ae
asimov <seyranmustafa4@gmail.com>
2024-08-30 20:25:30 +0300

    update

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,37 +1,46 @@
+
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import { doctorData } from "../helper/data";
 import AddModal from "./AddModal";
-import {useState} from "react"
+import { useState } from "react";
 
 const Doctors = () => {
-    const [modal, setModal] = useState(false);
-    const [selectedDoctor, setSelectedDoctor] = useState(null);
+  const [modal, setModal] = useState(false);
+  const [selectedDoctor, setSelectedDoctor] = useState(null);
 
   const handleModalOpen = (doctor) => {
     setSelectedDoctor(doctor);
-    setModal(true); 
+    setModal(true);
   };
 
   const handleModalClose = () => {
-    setModal(false); 
-    setSelectedDoctor(null); 
+    setModal(false);
+    setSelectedDoctor(null);
   };
   return (
     <>
       <h1 className="text-center">TAFAMUS HOSPİTAL</h1>
       <h2 className="text-center">Our Doctors</h2>
-      <Col xs={10} sm={8} md={6} lg={4} xl={3}>
-        {doctorData.map((doctor) => (
-          <Card key={doctor.id} className="my-2 mx-4 border-0">
-            <Card.Img onClick={()=>handleModalOpen(doctor)} variant="top" title={doctor.name} src={doctor.img} className="border border-5 rounded-2 " />
+
+      {doctorData.map((doctor) => (
+        <Col key={doctor.id} xs={10} sm={8} md={6} lg={4} xl={3}>
+          <Card className="my-2 mx-4 border-0">
+            <Card.Img
+              onClick={() => handleModalOpen(doctor)}
+              variant="top"
+              title={doctor.name}
+              src={doctor.img}
+              className="rounded-2 "
+            />
             <Card.Body>
               <Card.Title className="text-center">{doctor.name}</Card.Title>
               <Card.Text className="text-center">{doctor.dep}</Card.Text>
             </Card.Body>
           </Card>
-        ))}
-      </Col>
+        </Col>
+      ))}
+
       {modal && (
         <AddModal
           show={modal}
