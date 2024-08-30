e9aff6eb94cfb1c1cacf43b8ef9e97031f69b092
asimov <seyranmustafa4@gmail.com>
2024-08-30 16:46:10 +0300

    doctors

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -6,15 +6,15 @@ const Doctors = () => {
   console.log(doctorData);
   return (
     <>
-      <h1>TAFAMUS HOSPİTAL</h1>
-      <h2>Our Doctors</h2>
-      <Col xs={10} sm={8} md={6} lg={4} xl={3}>
+      <h1 className="text-center">TAFAMUS HOSPİTAL</h1>
+      <h2 className="text-center">Our Doctors</h2>
+      <Col xs={1} sm={2} md={3} lg={5}>
         {doctorData.map((doctor) => (
-          <Card key={doctor.id} style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={doctor.img} />
+          <Card key={doctor.id} className="my-2 mx-4 border-0">
+            <Card.Img variant="top" title={doctor.name} src={doctor.img} className="border border-5 rounded-2 " />
             <Card.Body>
-              <Card.Title>{doctor.name}</Card.Title>
-              <Card.Text>{doctor.dep}</Card.Text>
+              <Card.Title className="text-center">{doctor.name}</Card.Title>
+              <Card.Text className="text-center">{doctor.dep}</Card.Text>
             </Card.Body>
           </Card>
         ))}
