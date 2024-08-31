b7a3920dcc7adcf3784214c6cd5d0a9279e2dd4e
asimov <seyranmustafa4@gmail.com>
2024-08-31 11:45:55 +0300

    appointment

---
+++ src/components/AppointmentList.jsx
@@ -0,0 +1,11 @@
+import appointment from "../../public/img/appointment.jpg"
+
+const AppointmentList = () => {
+  return (
+    <div>
+        <img src={appointment} alt="" />
+    </div>
+  )
+}
+
+export default AppointmentList
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Row from "react-bootstrap/Row";
 import Doctors from "../components/Doctors";
 import Container from "react-bootstrap/Container";
+import AppointmentList from "../components/AppointmentList";
 
 const Home = () => {
   return (
     <Container>
       <Row>
         <Doctors />
+        <AppointmentList/>
       </Row>
     </Container>
   );
