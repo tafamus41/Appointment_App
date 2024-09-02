2ac6cc1b0cef70aed71a2379d032dc8731cf3715
asimov <seyranmustafa4@gmail.com>
2024-09-03 00:31:28 +0300

    p

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Doctors from "../components/Doctors";
 import Container from "react-bootstrap/Container";
 import AppointmentList from "../components/AppointmentList";
-
+import { appointmentData } from "../helper/data";
 const Home = () => {
+  const [appointments, setAppointments] = useState("")
+  const handleAdd = (newAppointment) => {
+    setAppointments([...appointments, newAppointment]);
+    localStorage.setItem(
+      "list",
+      JSON.stringify([...appointments, newAppointment])
+    );
+  };
   return (
     <Container>
       <Row>
-        <Doctors />
-        <AppointmentList/>
+        <Doctors handleAdd={handleAdd}/>
+        <AppointmentList appointments={appointments}/>
       </Row>
     </Container>
   );
